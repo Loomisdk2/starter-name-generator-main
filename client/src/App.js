78c175dd23d9efe9b7d9fdd0d781bed89cf5a3c9
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-10-17 12:31:47 -0500

    Fixed typo

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [name, setName] = useState([])
+  const [name, setName] = useState([]);
 
   useEffect(() => {
-    fetch('https://starter-name-generator-client-ephm.onrender.com') // Change this URL to the one that you just deployed
+    fetch("https://starter-name-generator-client-ephm.onrender.com") // Change this URL to the one that you just deployed
       .then((res) => res.json())
-      .then((name) => setName([name]))
+      .then((name) => setName([name]));
+  }, []);
 
-  return (
-    <h1>{name}</h1>
-  )
-}
+  return <h1>{name}</h1>;
+};
 
-export default App
+export default App;
